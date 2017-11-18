56687ef7ef2d4d390dba945ea30dba88a0f41ef0
StoykoBogev <stoiko.bogev@abv.bg>
2017-11-18 10:40:27 +0200

    Fixed SortCmd

--- ListProcessing/src/controller/SortCmd.java
+++ ListProcessing/src/controller/SortCmd.java
@@ -1,20 +1,12 @@
 package controller;
 
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-
 import static data.StringList.STRING_LIST;
 
 public class SortCmd extends Command {
-    private TreeSet<String> set;
-
-    public SortCmd() {
-        this.set = new TreeSet<>(STRING_LIST);
-    }
 
     @Override
     public void execute() {
-        STRING_LIST = this.set.stream().collect(Collectors.toList());
+        STRING_LIST.sort(String::compareTo);
         System.out.println((String.join(" ", STRING_LIST)));
     }
 }
